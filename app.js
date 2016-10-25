58eaef59f148f99cea51399ce3cc73e17f0a8cd8
Mohamed Nagy <mohamed.n.haleem@gmail.com>
2016-10-25 12:06:56 +0200

    intial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('static')); // serve static files from static folder
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+'/views/index.html');
+});
+
+
+io.on('connection', function (socket) {
+
+  console.log("client connected");
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+
+
+server.listen(3000);
